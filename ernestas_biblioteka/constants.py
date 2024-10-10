4009dfab9b3910adb2fec0bf114b67c8f28e1400
ErnestasZ <ernestas.zemaitis@gmail.com>
2024-10-10 10:08:35 +0300

    add save and load to Biblioteka

--- ernestas_biblioteka/constants.py
+++ ernestas_biblioteka/constants.py
@@ -4,3 +4,5 @@
 #                  'Biografijos ir autobiografijos', 'Receptų knygos', 'Istorinės knygos', 'Saviugda', 'Meno knygos', 'Kelionių knygos', 'Publicistika ir eseistika', 'Religinės knygos', 'Vaikų auklėjimo ir tėvystės knygos', 'Filosofija', 'Knygos apie sveikatą', 'Populiarioji psichologija', 'Mokslo populiarinimas', 'Verslo knygos']
 GENRES = ['Detektyvai', 'Fantastika', 'Distopijos ir utopijos', 'Istoriniai romanai', 'Siaubo romanai', 'Romantika, meilės romanai', 'Trumpos istorijos', 'Trileriai', 'Šiuolaikinė grožinė literatūra', 'Maginis realizmas', 'Jaunimo literatūra', 'Klasika', 'Grafinės novelės – komiksai', 'Vaikų literatūra',
           'Biografijos ir autobiografijos', 'Receptų knygos', 'Istorinės knygos', 'Saviugda', 'Meno knygos', 'Kelionių knygos', 'Publicistika ir eseistika', 'Religinės knygos', 'Vaikų auklėjimo ir tėvystės knygos', 'Filosofija', 'Knygos apie sveikatą', 'Populiarioji psichologija', 'Mokslo populiarinimas', 'Verslo knygos']
+
+LIB_FILE = 'biblioteka_data.pkl'
